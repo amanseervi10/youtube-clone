134e2d1aa43dd63e41873afd7e99f54f58fec185
Aman Seervi <amanseervi10@gmail.com>
2023-04-10 11:18:56 +0530

    main layout and basic api setup

---
+++ src/components/Videos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Stack , Box} from '@mui/material';
+
+const Videos = ({videos}) => {
+  return (
+    <Stack sx={{flexDirection:{sx:'column', md:'row'}}}>
+      
+    </Stack>
+  )
+}
+
+export default Videos
